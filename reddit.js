bd440226f2099f8ceb251383049c90813b5039d0
Maurice Svay <maurice@svay.com>
2012-07-08 21:53:53 +0200

    Delete trailing whitespace, jslint

--- reddit.js
+++ reddit.js
@@ -7,11 +7,11 @@ var Reddit = {
     subreddit : '/r/pics/new/',
     feed : null,
     first : null,
-    
+
     init: function() {
-    
+
         Reddit.feed = 'http://www.reddit.com' + Reddit.subreddit + '.json?format=json&sort=new';
-    
+
         $.ajax({
             dataType : 'jsonp',
             jsonp : 'jsonp',
@@ -20,26 +20,26 @@ var Reddit = {
                 Reddit.append(data);
             }
         });
-        
+
         //Handle item selection
         $('#items').delegate('a','click', function(e) {
             e.preventDefault();
-            
+
             var $target = $(e.currentTarget);
             var url = $target.attr('href');
-            
+
             //Is this a load more link ?
             if ('next' == $target.attr('rel')) {
                 Reddit.loadMore($target);
                 return;
             }
-            
+
             if (Reddit.$current) {
                 Reddit.$current.removeClass('selected');
             }
             $target.addClass('selected');
             Reddit.$current = $target;
-            
+
             if (url.match(/(png|jpg|jpeg|gif)$/i)) {
                 $('#details').html('<img src="' + url + '">');
             } else {
@@ -53,7 +53,7 @@ var Reddit = {
                 }
             }
             $('#status').html('<a href="'+$(this).data('source')+'">'+$(this).data('title')+'</a>');
-            
+
             //Scroll
             var position = $(this).position();
             var scroll = $('#items').scrollTop();
@@ -61,9 +61,9 @@ var Reddit = {
             //$('#items').scrollTop(scroll + position.top - height/2);
             $('#items').animate({
                 scrollTop: scroll + position.top - height/2
-            })
+            });
         });
-        
+
         //Next button (could certainly be refactored)
         $('#next').bind('click', function() {
             if (Reddit.$current) {
@@ -75,7 +75,7 @@ var Reddit = {
                 }
             }
         });
-        
+
         //Previous button (could certainly be refactored)
         $('#prev').bind('click', function() {
             if (Reddit.$current) {
@@ -87,17 +87,21 @@ var Reddit = {
                 }
             }
         });
-        
-        $(document).bind('keydown', 'up', function() {$('#prev').trigger('click')});
-        $(document).bind('keydown', 'down', function() {$('#next').trigger('click')});
+
+        $(document).bind('keydown', 'up', function() {
+            $('#prev').trigger('click');
+        });
+        $(document).bind('keydown', 'down', function() {
+            $('#next').trigger('click');
+        });
     },
-    
+
     loadMore : function(elem) {
         $("#status").html("<strong>Loading...</strong>");
-        
+
         var placeholder = elem.parent('li');
         var url = elem.attr('href');
-    
+
         $.ajax({
             dataType : 'jsonp',
             jsonp : 'jsonp',
@@ -107,41 +111,41 @@ var Reddit = {
             }
         });
     },
-    
+
     append : function(data, placeholder) {
         var items = data.data.children;
         var all = [];
         var after = data.data.after;
-        
+
         if (items.length) {
             if (placeholder) {
                 $(placeholder).remove();
             }
-            
+
             var fragment = document.createDocumentFragment();
-        
+
             for (var i=0,l=items.length; i<l; i++) {
-                if (i == 0) {
+                if (i === 0) {
                     Reddit.first = items[i].data.url;
                 }
-                
+
                 var a = $('<a/>');
                 a.attr("href", items[i].data.url);
                 a.data("source", "http://www.reddit.com" + items[i].data.permalink);
                 a.data("title", items[i].data.title);
                 a.html('<img width="70" src="' + items[i].data.thumbnail + '" alt=""> ' + items[i].data.title);
-                
+
                 var li = $('<li/>');
                 li.append(a);
-                
+
                 fragment.appendChild(li[0]);
             }
             $("#items").append(fragment);
             $("#items").append('<li><a rel="next" href="'+Reddit.feed+'&amp;after=' + after +  '">Load more</a></li>');
-            
+
             $("a[href='" + Reddit.first + "']").trigger('click');
         } else {
             alert("Error loading more");
         }
     }
-}
+};
